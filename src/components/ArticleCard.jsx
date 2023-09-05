1a58ece85247f98c9e5e66c475bff22c61ba2193
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2023-09-05 13:20:12 +0200

    add styles and font

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,11 +1,16 @@
 const ArticleCard = ({ article }) => {
+const createdAtString = article.created_at;
+const formattedCreatedAt = createdAtString.substring(0, 10);
+const articleTitle = article.title;
+const lowercaseTitle = articleTitle.toLowerCase();
+
   return (
-    <li className="article-item" key={article.article_id} >
-      <h2>{article.title}</h2>
-      <p>Author: {article.author}</p>
-      <p>Published: {article.created_at}</p>
-      <p>Votes: {article.votes}</p>
+    <li className="article-item" key={article.article_id}>
+      <h2 className="article-title">{lowercaseTitle}</h2>
       <img className="article-img" src={article.article_img_url} />
+      <p>Author: {article.author}</p>
+      <p>Published: {formattedCreatedAt }</p>
+      <p>Votes: {article.votes}</p>
     </li>
   );
 };
