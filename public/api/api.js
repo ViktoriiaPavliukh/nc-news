c4acc5af4b30cb3612825ac5c8b8cab4e04ffe24
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2023-09-06 12:27:45 +0100

    create opening of Single ArticleCard using react-router-dom

--- public/api/api.js
+++ public/api/api.js
@@ -4,3 +4,15 @@ const BASE_URL = "https://news-api-sgzy.onrender.com/api";
 export const getArticles = () => {
   return axios.get(`${BASE_URL}/articles`);
 };
+
+export const getArticleById = (article_id) => {
+  return axios
+    .get(`${BASE_URL}/articles/${article_id}`)
+    .then((response) => {
+      console.log("API Response:", response.data.article); 
+      return response.data.article;
+    })
+    .catch((error) => {
+      throw new Error("Failed to fetch article: " + error.message);
+    });
+};
--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { getArticles } from "../../public/api/api";
 import ArticleCard from "./ArticleCard";
 
@@ -12,11 +13,20 @@ const ArticlesList = () => {
     return (
       <ul className="article-list">
         {articles.map((article) => {
-          return <ArticleCard key={article.article_id} article={article} />;
+          return (<Link to={`/articles/${article.article_id}`} key={article.article_id}>
+            <li article={article} className="article-item" key={article.article_id}>
+            <h2 className="article-title">{article.title}</h2>
+            <img className="article-img" src={article.article_img_url} alt="image of article"/>
+            <p>Author: {article.author}</p>
+            <p>Published: {article.created_at }</p>
+            <p>Votes: {article.votes}</p>
+          
+          </li>
+         {/* <ArticleCard /> */}
+          </Link>
+          );
         })}
       </ul>
     );
-
-  }
-
+}
   export default ArticlesList;
